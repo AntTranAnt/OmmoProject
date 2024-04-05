bacab92326011cbdf717c16b62154cee1987d129
AntTranAnt <tranant2002@gmail.com>
2024-04-05 15:43:50 -0500

    finished avg position

--- test.py
+++ test.py
@@ -10,6 +10,28 @@ import numpy as np
 import sys
 import os
 
+# Dictionary to represent stored data
+# data has keys=filename, value=list<deviceDict>
+# deviceDict has keys=device[sensor#], value=list<sensor avg> for avg
+# deviceDict has keys=device[sensor#], value=maxDistance for distance
+class OutputDictionary:
+    def __init__(self):
+        self.data = {}
+    
+    def addFile(self, filename):
+        self.data[filename] = []
+    
+    def addDeviceAvg(self, filename, deviceName, avgList):
+        deviceDict = {deviceName: avgList}
+        self.data[filename].append(deviceDict)
+
+    def addDeviceDistance(self, filename, deviceName, maxDistance):
+        deviceDict = {deviceName: maxDistance}
+        self.data[filename].append(deviceDict)
+    
+    def __str__(self):
+        return str(self.data)
+
 # Checks if argument is valid with input folder and output folder
 # returns list of files if valid
 # returns empty list if not
@@ -30,20 +52,22 @@ def inputValidation(inputFolder, outputFolder) -> list:
     
     return fileList
 
-# Computes the average position for each sensor on each device in all valid files
-def computeAveragePosition(inputFolder, outputFolder, fileList):
+# Loops through all files and finds sensor dataset from all valid 'Position' datasets
+# file.hdf5 -> list<devices> -> device -> list<datasets> -> position dataset -> sensor dataset
+def compute(avgOutput, inputFolder, outputFolder, fileList):
     output = dict() #key = file name, maps to list of x, y, z data
     # Loop through files in folder
     for file in fileList:
         filePath = inputFolder + '/' + file
         # Checks if file is a .hdf5 file
         if filePath.endswith('.hdf5'):
-            file = h5py.File(filePath, 'r')
-            deviceList = list(file.keys())
+            fileH = h5py.File(filePath, 'r')
+            deviceList = list(fileH.keys())
+            avgOutput.addFile(file)
             # Loops through device groups in file
             for device in deviceList:
                 # Checks if device has Position dataset
-                tempDevice = file[device]
+                tempDevice = fileH[device]
                 if 'Position' in tempDevice:
                     positionDataset = tempDevice['Position']
                     shape = positionDataset.shape
@@ -51,8 +75,27 @@ def computeAveragePosition(inputFolder, outputFolder, fileList):
                     sensorIndex = 1
                     for i in range(shape[sensorIndex]):
                         sensorDataset = positionDataset[:, i, :]
-                        print(sensorDataset)
+                        deviceName = device + "[" + str(i) + "]"
+                        computeAvgPosition(avgOutput, sensorDataset, file, deviceName)
 
+# Computes the average position for each sensorDataset and inputs to output dictionary
+def computeAvgPosition(avgOutput, sensorDataset, filename, deviceName):
+    sampleNum = 0
+    xSum = 0
+    ySum = 0
+    zSum = 0
+    for sample in sensorDataset:
+        sampleNum += 1
+        xSum += sample[0]
+        ySum += sample[1]
+        zSum += sample[2]
+    
+    # Checks if sample is empty, if not then append results to avgOutput
+    if sampleNum != 0:
+        xSum /= sampleNum
+        ySum /= sampleNum
+        zSum /= sampleNum
+        avgOutput.addDeviceAvg(filename, deviceName, [xSum, ySum, zSum])
 
 def main():
     # Checks for proper input
@@ -63,25 +106,11 @@ def main():
     fileList = inputValidation(inputFolder, outputFolder)
     if len(fileList) == 0: return
 
-    computeAveragePosition(inputFolder, outputFolder, fileList)
-
-    # f = h5py.File('1.hdf5', 'r')
-    # # List of device groups
-    # deviceList = list(f.keys())
-    # print(deviceList)
-    # # Device 1
-    # device = f[deviceList[0]]
-    # print(device)
-    # # Position dataset of device 1
-    # dataset = device['Position']
-    # print(dataset)
-    # # Dataset of sample 1 of device 1
-    # sample1Dataset = dataset[:, 0, :]
-    # print(sample1Dataset)
-    # for sample in sample1Dataset:
-    #     print(sample[0])
-    #     print(sample[1])
-    #     print(sample[2])
+    # Dictionary to keep track of results
+    avgOutput = OutputDictionary()
+    distOutput = OutputDictionary()
+    compute(avgOutput, inputFolder, outputFolder, fileList)
+    print(avgOutput)
 
 if __name__ == "__main__":
     main()
